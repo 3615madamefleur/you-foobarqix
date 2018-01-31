c82708f05407df778eed0b177f75f94f82b7b665
You <thibaultyou@gmail.com>
2018-02-01 00:32:00 +0100

    Adding AssertJ tests

--- src/test/java/fbq/FooBarQixTest.java
+++ src/test/java/fbq/FooBarQixTest.java
@@ -1,30 +1,91 @@
 package fbq;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.*;
 
 /**
  * FooBarQix test class
  */
 public class FooBarQixTest {
-    private FooBarQix test;
+    private final int min = 1;
+    private final int max = 100;
+    private FooBarQix fbq;
+    private Map<Integer, String> mock;
 
     @Before
-    public void reload() {
-        test = new FooBarQix(1, 100);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testExceptions() {
-        test = new FooBarQix(-5, 68);
-        test = new FooBarQix(156, 42);
+    public void arrange() {
+        fbq = new FooBarQix(min, max);
+        mock = new LinkedHashMap<>();
+        IntStream.range(min, max + 1)
+                .forEach(i -> mock.put(i, fbq.transform(i)));
     }
 
     @Test
-    public void test() {
-        Assert.assertEquals("FooFooFoo", test.transform(33));
-        Assert.assertEquals("QixQix", test.transform(7));
-        Assert.assertEquals("FooBarQix", test.transform(57));
+    public void should_throw_exception_when_args_are_under_zero() {
+        Throwable thrown = catchThrowable(() -> {
+            FooBarQix exception = new FooBarQix(-5, 68);
+        });
+        assertThat(thrown).isInstanceOf(IllegalArgumentException.class)
+                .hasNoCause();
+    }
+
+    @Test
+    public void should_throw_exception_when_args_are_reversed() {
+        Throwable thrown = catchThrowable(() -> {
+            FooBarQix exception = new FooBarQix(156, 42);
+        });
+        assertThat(thrown).isInstanceOf(IllegalArgumentException.class)
+                .hasNoCause();
+    }
+
+    @Test
+    public void should_contains_foo_when_divisible_by_3() {
+        IntStream.range(min, max + 1).filter(i -> i % FooBarQix.FOO == 0).forEach(i -> {
+            assertThat(mock.get(i)).contains("Foo");
+        });
+    }
+
+    @Test
+    public void should_contains_bar_when_divisible_by_5() {
+        IntStream.range(min, max + 1).filter(i -> i % FooBarQix.BAR == 0).forEach(i -> {
+            assertThat(mock.get(i)).contains("Bar");
+        });
+    }
+
+    @Test
+    public void should_contains_qix_when_divisible_by_7() {
+        IntStream.range(min, max + 1).filter(i -> i % FooBarQix.QIX == 0).forEach(i -> {
+            assertThat(mock.get(i)).contains("Qix");
+        });
+    }
+
+    @Test
+    public void should_contains_foo_when_a_digit_equals_3() {
+        IntStream.range(min, max + 1).forEach(i -> String.valueOf(i).chars()
+                .mapToObj(Character::getNumericValue).filter(e -> e == FooBarQix.FOO).forEach(e -> {
+                    assertThat(mock.get(i)).contains("Foo");
+                }));
+    }
+
+    @Test
+    public void should_contains_bar_when_a_digit_equals_5() {
+        IntStream.range(min, max + 1).forEach(i -> String.valueOf(i).chars()
+                .mapToObj(Character::getNumericValue).filter(e -> e == FooBarQix.BAR).forEach(e -> {
+                    assertThat(mock.get(i)).contains("Bar");
+                }));
+    }
+
+    @Test
+    public void should_contains_qix_when_a_digit_equals_7() {
+        IntStream.range(min, max + 1).forEach(i -> String.valueOf(i).chars()
+                .mapToObj(Character::getNumericValue).filter(e -> e == FooBarQix.QIX).forEach(e -> {
+                    assertThat(mock.get(i)).contains("Qix");
+                }));
     }
 }
